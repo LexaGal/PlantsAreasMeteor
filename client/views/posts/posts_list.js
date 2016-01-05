68faf735c31a7ff9287d1e3c82bd1acd63d3ee6a
Galushkin Aleksey <galushkin.aleksey@gmail.com>
2016-01-05 17:01:03 +0300

    Added data to the posts collection.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -2,11 +2,11 @@ var postsData = [
   {
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
-  }, 
+  },
   {
     title: 'Meteor',
     url: 'http://meteor.com'
-  }, 
+  },
   {
     title: 'The Meteor Book',
     url: 'http://themeteorbook.com'
