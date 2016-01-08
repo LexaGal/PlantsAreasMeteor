394bc6797f66058e2f150c967b888c84118eb6a9
Galushkin Aleksey <galushkin.aleksey@gmail.com>
2016-01-08 14:21:10 +0300

    Показываем комментарии на странице с постом.

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,18 +4,18 @@ if (Posts.find().count() === 0) {
 
     // create two users
     var tomId = Meteor.users.insert({
-        profile: { name: 'Tom Coleman' }
+        profile: { first_name: 'Tom',  last_name: 'Coleman' }
     });
     var tom = Meteor.users.findOne(tomId);
     var sachaId = Meteor.users.insert({
-        profile: { name: 'Sacha Greif' }
+        profile: { first_name: 'Sacha', last_name: 'Greif' }
     });
     var sacha = Meteor.users.findOne(sachaId);
 
     var telescopeId = Posts.insert({
         title: 'Introducing Telescope',
         userId: sacha._id,
-        author: sacha.profile.name,
+        author: sacha.profile.first_name.concat(" ").concat(sacha.profile.last_name),
         url: 'http://sachagreif.com/introducing-telescope/',
         submitted: now - 7 * 3600 * 1000
     });
@@ -23,7 +23,7 @@ if (Posts.find().count() === 0) {
     Comments.insert({
         postId: telescopeId,
         userId: tom._id,
-        author: tom.profile.name,
+        author: tom.profile.first_name.concat(" ").concat(tom.profile.last_name),
         submitted: now - 5 * 3600 * 1000,
         body: 'Interesting project Sacha, can I get involved?'
     });
@@ -31,7 +31,7 @@ if (Posts.find().count() === 0) {
     Comments.insert({
         postId: telescopeId,
         userId: sacha._id,
-        author: sacha.profile.name,
+        author: sacha.profile.first_name.concat(" ").concat(sacha.profile.last_name),
         submitted: now - 3 * 3600 * 1000,
         body: 'You sure can Tom!'
     });
@@ -39,7 +39,7 @@ if (Posts.find().count() === 0) {
     Posts.insert({
         title: 'Meteor',
         userId: tom._id,
-        author: tom.profile.name,
+        author: tom.profile.first_name.concat(" ").concat(tom.profile.last_name),
         url: 'http://meteor.com',
         submitted: now - 10 * 3600 * 1000
     });
@@ -47,7 +47,7 @@ if (Posts.find().count() === 0) {
     Posts.insert({
         title: 'The Meteor Book',
         userId: tom._id,
-        author: tom.profile.name,
+        author: tom.profile.first_name.concat(" ").concat(tom.profile.last_name),
         url: 'http://themeteorbook.com',
         submitted: now - 12 * 3600 * 1000
     });
