1755f37079a7e2a757dfe264fb731c6f79a829df
Aliaksei Halushkin <aliaksei_halushkin@epam.com>
2016-10-13 19:27:38 +0300

    Started...

---
+++ IdentityMySQL/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityMySQL.Startup))]
+namespace IdentityMySQL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
+
